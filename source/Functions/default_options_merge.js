ff7a912b046e9317743f87655735b8cb9f6cbcb0
Matt Harris <mjharris2407@gmail.com>
2019-04-10 21:19:50 -0400

    updated js functions

--- source/Functions/default_options_merge.js
+++ source/Functions/default_options_merge.js
@@ -13,33 +13,12 @@ function Module(options = {}){
   console.info( actual.color );
 }
 
-// DEFAULT PARAMETERS
-// ------------------
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/default_parameters
-// Default function parameters allow formal parameters to be initialized with default values if no value or undefined is passed.
-// ```
-// function [name]([param1[ = defaultValue1 ][, ..., paramN[ = defaultValueN ]]]) {
-//   statements
-// }
-// ```
-function multiply(a, b = 1) {
-  return a * b;
-}
-
-console.log(multiply(5, 2));
-// expected output: 10
-
-console.log(multiply(5));
-// expected output: 5
-
-
-
 
 // ES6 Defaults / Overrides Pattern
 // --------------------------------
 // Combine default parameters and destructuring for a compact version of the defaults / overrides pattern.
 // https://gist.github.com/ericelliott/f3c2a53a1d4100539f71#es6-defaults--overrides-pattern
-function foo ({
+function merge_defaultsA ({
     bar = 'no',
     baz = 'works!'
   } = {}) {
@@ -47,23 +26,17 @@ function foo ({
   return (`${bar}, ${baz}`);
 }
 
-console.log(foo({
-  bar: 'yay'
-})); // logs 'yay, works!'
+// console.log(merge_defaultsA({ bar: 'yay' }));
 
 
-var assign = Object.assign;
 
-var defaults2 = {
+function merge_defaultsB (options) {
+  var assign = Object.assign;
+
+  var defaults2 = {
     bar: 'no',
     baz: 'works!'
   };
-
-// ES5 Equivalent
-// --------------
-
-
-function foo2 (options) {
   var settings = assign({}, defaults2, options),
     bar = settings.bar,
     baz = settings.baz;
@@ -71,7 +44,4 @@ function foo2 (options) {
   return (bar + ', ' +baz);
 }
 
-console.log(foo2({
-  bar: 'yay'
-})); // logs 'yay, works!'
-Gist written for How to Use ES6 for Isomorphic JavaScript Apps
+// console.log(merge_defaultsB({  bar: 'yay'}));
